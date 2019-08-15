c5081e1403562970993da5e7c6482cf21a6e0d4b
MattisBrizard <mattisbrizard@icloud.com>
2019-08-15 20:37:46 +0200

    updated tests

--- test/circular_countdown_painter_test.dart
+++ test/circular_countdown_painter_test.dart
@@ -4,11 +4,11 @@ import 'package:circular_countdown/src/circular_countdown_painter.dart';
 
 void main() {
   test('CircularCountdownPainter currentPaint should be null', () {
-    CircularCountdownPainter painter = CircularCountdownPainter(
+    final CircularCountdownPainter painter = CircularCountdownPainter(
       countdownTotal: 10,
       countdownRemaining: 6,
-      countdownTotalColor: Color(0X4DFFFFFF),
-      countdownRemainingColor: Color(0XFFFFFFFF),
+      countdownTotalColor: const Color(0X4DFFFFFF),
+      countdownRemainingColor: const Color(0XFFFFFFFF),
       strokeWidth: 40,
       gapFactor: 2,
     );
@@ -16,17 +16,17 @@ void main() {
     expect(painter.currentPaint, null);
   });
   test('CircularCountdownPainter currentPaint should be correct', () {
-    CircularCountdownPainter painter = CircularCountdownPainter(
+    final CircularCountdownPainter painter = CircularCountdownPainter(
       countdownTotal: 10,
       countdownRemaining: 6,
-      countdownTotalColor: Color(0X4DFFFFFF),
-      countdownRemainingColor: Color(0XFFFFFFFF),
-      countdownCurrentColor: Color(0XFFF4CAF5),
+      countdownTotalColor: const Color(0X4DFFFFFF),
+      countdownRemainingColor: const Color(0XFFFFFFFF),
+      countdownCurrentColor: const Color(0XFFF4CAF5),
       strokeWidth: 40,
       gapFactor: 2,
     );
 
-    expect(painter.currentPaint.color, Color(0XFFF4CAF5));
+    expect(painter.currentPaint.color, const Color(0XFFF4CAF5));
     expect(painter.currentPaint.strokeWidth, 40);
     expect(painter.currentPaint.style, PaintingStyle.stroke);
   });
