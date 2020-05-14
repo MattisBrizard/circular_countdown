7e6a0a5386b68d1479909263108b1dfcf0f6e91e
MattisBrizard <MattisBrizard@users.noreply.github.com>
2020-05-14 17:50:50 +0000

    deploy: 6009c0f452fa86e4a307204e1efc6d5799b17139

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,18 +1,17 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "926cd8008bef60a4e735ba8d18fbac9c",
-"/": "926cd8008bef60a4e735ba8d18fbac9c",
+  "main.dart.js": "7cb53c141c6ea3944ac8a8d37812bdf3",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"assets/LICENSE": "a52f68c84fbc29aa05b00a24001ab266",
+"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/LICENSE": "4de04d24b0b1e0af094341344eff7f3c",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "c0591ae297661b17dd7d63607a262f69",
-"manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49"
+"index.html": "926cd8008bef60a4e735ba8d18fbac9c",
+"/": "926cd8008bef60a4e735ba8d18fbac9c"
 };
 
 self.addEventListener('activate', function (event) {
